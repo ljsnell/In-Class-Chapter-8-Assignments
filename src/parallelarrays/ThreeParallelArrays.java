1bfcc8216b5c16b23b413b47a8355b7c416c2084
ljsnell <ljsnell7@gmail.com>
2022-10-09 17:38:26 -0500

    Update ThreeParallelArrays.java
    Updated with initial variables.

--- src/parallelarrays/ThreeParallelArrays.java
+++ src/parallelarrays/ThreeParallelArrays.java
@@ -3,7 +3,8 @@ package parallelarrays;
 public class ThreeParallelArrays {
 
 	public static void main(String[] args) {
-
+		String states[] = {"Alabama"};
+		String statesAbbreviation[] = {"AL"};
+		String capitals[] = {"Montgomery"};
 	}
-
 }
